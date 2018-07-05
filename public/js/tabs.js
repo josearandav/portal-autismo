24745302de4ac0a5c9fd17f916b42aa42e68622c
José Aranda Vera <ronnycaturro@gmail.com>
2018-07-05 18:09:34 -0400

    correccion public

---
+++ public/js/tabs.js
@@ -0,0 +1,23 @@
+function toggleTab(selectedNav, targetId) {
+  var navEls = document.querySelectorAll("#nav li");
+
+  navEls.forEach(function(navEl) {
+    if (navEl.id == selectedNav) {
+      navEl.classList.add("is-active");
+    } else {
+      if (navEl.classList.contains("is-active")) {
+        navEl.classList.remove("is-active");
+      }
+    }
+  });
+
+  var tabs = document.querySelectorAll(".tab-pane");
+
+  tabs.forEach(function(tab) {
+    if (tab.id == targetId) {
+      tab.style.display = "block";
+    } else {
+      tab.style.display = "none";
+    }
+  });
+}
