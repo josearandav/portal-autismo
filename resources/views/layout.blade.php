541f4b62e2c16bec31de60f01e7d6872a221e106
José Aranda Vera <ronnycaturro@gmail.com>
2018-07-05 14:55:35 -0400

    correccion layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Tea-Acompaño</title>
-    <link rel="shortcut icon" href="../images/fav_icon.png" type="image/x-icon">
+ <title>Hero - Free Bulma template</title>
+        <link rel="shortcut icon" href="../images/fav_icon.png" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <!-- Bulma Version 0.7.1-->
@@ -23,43 +24,44 @@
             <a class="navbar-item" href="../">
               <img src="http://www.espiralsantacruz.com/wp-content/uploads/2017/02/ada75c21274f58497cd22cda18cadf15.png" alt="Logo">  
             </a>
-            <!--menu responsivo-->
-            <span class="navbar-burger burger" data-target="navbarMenu">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          </div>
+       
+            <!--Menu responsivo-->
+                <span class="navbar-burger burger" data-target="navbarMenu">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
 
-          <div id="navbarMenu" class="navbar-menu">
-            <div class="navbar-end">
+              <div id="navbarMenu" class="navbar-menu">
+              <div class="navbar-end">
               <div class="tabs is-right">
                 <ul>
-                <li><a class="navbar-item" href="inicio">
+                <li class="is-active"><a href="inicio">
                 Inicio
               </a></li>
-                <li><a class="navbar-item" href="about">
+                <li><a href="about">
                 Sobre nosotros
               </a></li>
               @auth
-                <li><a class="navbar-item" href="https://josearandav.github.io/Tea-Docs/docs/">
+                <li><a href="https://josearandav.github.io/Tea-Docs/docs/">
                 Zona de descargas
               </a></li>
               @endauth
               @guest
-                <li><a class="navbar-item" href="register">
+                <li><a href="register">
                 Registro
               </a></li>
 
-                <li><a class="navbar-item" href="login">
+                <li><a href="login">
                 Login
               </a></li>
               @endguest
               @auth
-                <li><a class="navbar-item" href="/">
+                <li><a href="/">
                 Deslogear
               </a></li>
               @endauth
@@ -99,6 +101,7 @@
                 </a>
               </span>
             </div>
+            </div>
           </div>
         </div>
       </nav>
@@ -108,7 +111,7 @@
         <h1 class="title">
           TEA-acompaño &lt;Entre mayor el desafío mayor es la victoria&gt;
         </h1>
-        <img src="http://saludiario.grupodecomunicac.netdna-cdn.com/wp-content/uploads/2016/10/autismo-730x487.jpg" alt="Autismo" width="500" height="300">
+        <img src="http://saludiario.grupodecomunicac.netdna-cdn.com/wp-content/uploads/2016/10/autismo-730x487.jpg" alt="Autismo" width="300" height="50">
         <h2 class="subtitle"> Juntos hacia el futuro!
         </h2>
       </div>
@@ -126,7 +129,7 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>Tea-Acompaño</strong> by <a href="https://josearandav.github.io/">José Aranda Vera</a>. The source code is licensed
+          <strong>Tea-Acompaño</strong> by <a href="https://josearandav.github.io/">José Aranda Vera</a>. Todos los derechos de esta pagina tienen licencia
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
         </p>
         <p>
@@ -138,6 +141,8 @@
     </div>
   </footer>
   <script async type="text/javascript" src="../js/bulma.js"></script>
+  <script src="../js/bulma.js"></script>
 </body>
 </html>
 
+ 
