dc8e886e116c43350a5ec976fb8a302ad076643f
José Aranda Vera <ronnycaturro@gmail.com>
2018-09-06 17:41:54 -0300

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,23 @@
 
 @section('content')
 
+    <div class="intro column is-8 is-offset-2">
+      <h2 class="title">Quienes somos?</h2><br>
+      <p class="subtitle">Quienes somos?
+
+Somos un grupo de padres, familiares, personas que a diario convivimos con el autismo, y sabemos lo difícil que es enfrentar esta situación. Es por lo mismo que queremos a través de esta plataforma web, poder llegar a cada uno de ustedes para contarles de nuestra experiencia y ayudar a  sobrellevar esta difícil pero hermosa tarea, y tener éxito en los logros que podamos llegar a tener.
+<br>
+La idea de esta aplicación web, es generar comunidad y por ese medio llegar a tener un apoyo mutuo, entre los integrantes, muchas veces solamente necesitamos ser escuchados, o no sabemos por donde comenzar. 
+<br>
+Quedan todos invitados a participar activamente de esta comunidad y ser parte de nuestro sueño en común, es que es dar mejor calidad de vida para nuestros amados “Ángeles Azules”.  
+<br>
+<br>
+
+Una cordial bienvenida y fuerte abrazo a la distancia del equipo de TEA-Compaño.
+<br>
+<br>
+“ENTRE MAYOR ES EL DESAFIO, MAS GRANDE SERA NUESTRA VICTORIA”
+</p>
+</div>
 
 @stop
